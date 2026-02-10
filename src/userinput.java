52b43638ccddf8a555c453a28c2d0cd1354b5197
Saikat Biswas <saikatbiswas24dev@gmail.com>
2026-02-10 21:30:39 +0530

    user input practice

---
+++ src/userinput.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class userinput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("Are you single ?");
+        boolean single = sc.nextBoolean();
+        System.out.println("my name is "+name);
+        System.out.println("My age is "+age);
+        System.out.println("Are you single ?"+single);
+    }
+}
