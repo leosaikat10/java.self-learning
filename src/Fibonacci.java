ca3d02449588e77ddb35fcde0bdc8af2616d00b2
Saikat Biswas <saikatbiswas24dev@gmail.com>
2026-03-01 20:59:27 +0530

    Fibonacci Series

---
+++ src/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int a  = sc.nextInt();
+        System.out.println("Enter the other number : ");
+        int b  = sc.nextInt();
+        System.out.println("Till the number ? :");
+        int n = sc.nextInt();
+
+        System.out.println("The Series is :");
+        for(int i = 1; i<=n; i++ ) {
+            System.out.println(a + ",");
+            int c = a + b ;
+            a = b ;
+            b = c ;
+        }
+    }
+}
